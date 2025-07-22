fa8e8cf71052a3cb7737660602b473a25b16f187
Однолетков Максим Михайлович <odnoletkov.maksim@itmh.ru>
2025-07-22 19:42:29 +0500

    fix unused var

--- src/components/PauseableTimer/PauseableTimer.jsx
+++ src/components/PauseableTimer/PauseableTimer.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import styles from './PauseableTimer.module.css';
+import './PauseableTimer.module.css';
 
 const PauseableTimer = () => {
   const initSeconds = 0;
