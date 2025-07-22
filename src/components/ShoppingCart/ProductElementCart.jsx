7816f3df7d3306dc5006413c9e8cdbf6a28fb8dd
Однолетков Максим Михайлович <odnoletkov.maksim@itmh.ru>
2025-07-22 12:49:00 +0500

    removed the extra hooks

--- src/components/ShoppingCart/ProductElementCart.jsx
+++ src/components/ShoppingCart/ProductElementCart.jsx
@@ -1,33 +1,29 @@
-import { useState,useEffect } from 'react';
 import styles from './ShoppingCart.module.css';
 
 // Карточка продукта в списке корзины
 const ProductElementCart = ({product, onChange, onDelete}) => {
-  
-  const [productSum, setProductSum] = useState(product.sum);
-  useEffect(() => {setProductSum(product.sum)},[product]);
 
   return <div className={`${styles.shopping_cart__product}`}>
     <div>{product.name}</div>
     <div>
       <input type="button" value="-" onClick={() => {
-        if (productSum > 1) {
-          setProductSum(productSum - 1);
-          onChange(product.id,productSum - 1);
+        if (product.sum > 1) {
+          document.getElementById('productCounter').value = product.sum - 1;
+          onChange(product.id,product.sum - 1);
         }
         }} />
-      <input type="number" className={`${styles.product_counter}`} value={productSum} onChange={e => {
-          if (e.target.value > 1) {
-            setProductSum(+e.target.value);
+      <input type="number" id='productCounter' className={`${styles.product_counter}`} value={product.sum} onChange={e => {
+          if (e.target.value > 1) {            
+            document.getElementById('productCounter').value = +e.target.value;
             onChange(product.id,+e.target.value);
           } else {
-            setProductSum(1);
+            document.getElementById('productCounter').value = 1;
             onChange(product.id,1);
           }
         }} />
       <input type="button" value="+" onClick={() => {
-        setProductSum(productSum + 1);
-        onChange(product.id,productSum + 1);
+        document.getElementById('productCounter').value = product.sum + 1;
+        onChange(product.id,product.sum + 1);
         }} />
     </div>
     <input type="button" value="×" onClick={() => {onDelete(product.id)}} />
