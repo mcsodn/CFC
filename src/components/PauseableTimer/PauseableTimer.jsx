bb8d38909e2f1afae6e8bddb22bd012839b29431
Однолетков Максим Михайлович <odnoletkov.maksim@itmh.ru>
2025-07-22 17:07:04 +0500

    add timer logic

--- src/components/PauseableTimer/PauseableTimer.jsx
+++ src/components/PauseableTimer/PauseableTimer.jsx
@@ -2,17 +2,38 @@ import { useState, useEffect } from 'react';
 import styles from './PauseableTimer.module.css';
 
 const PauseableTimer = () => {
-  
-  const [seconds,setSeconds] = useState(30);
+  const initSeconds = 0;
+  const [seconds,setSeconds] = useState(initSeconds);
+  const [isStopped,setIsStopped] = useState(true);
+
+  const startTimer = () => {
+    setIsStopped(false);
+  }
+
+  const pauseTimer = () => {
+    setIsStopped(true);
+  }
+
+  const restartTimer = () => {
+    setIsStopped(true);
+    setSeconds(initSeconds);
+  }
+
   useEffect(() => {
-    
-  }, [seconds]);
+    while (!isStopped) {
+      const interval = setInterval(() => {
+        setSeconds(prevSeconds => prevSeconds + 1)
+      }, 1000);
+
+      return () => clearInterval(interval);
+    }
+  }, [isStopped, seconds]);
 
   return <div>
       <h1>{seconds}</h1>
-      <input type="button" value="Старт" />
-      <input type="button" value="Пауза" />
-      <input type="button" value="Заново" />
+      <input type="button" value="Старт" onClick={startTimer} />
+      <input type="button" value="Пауза" onClick={pauseTimer}/>
+      <input type="button" value="Заново" onClick={restartTimer}/>
     </div>
 }
 
